f65e623f008fac6c374f65f4d112b20ccc9d57bc
Loris Cassarà <cassaralavoro@gmail.com>
2024-02-23 16:26:32 +0100

    added models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -23,4 +23,9 @@ class Product extends Model
     {
         return $this->belongsTo(Restaurant::class);
     }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
 }
