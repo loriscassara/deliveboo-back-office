e3afbb580a5c54846e8557a8b7de134b5031db86
Loris Cassarà <cassaralavoro@gmail.com>
2024-02-26 15:40:04 +0100

    test for pull

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,4 +24,10 @@
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
     </div>
 </div>
+
+<div class="content">
+    <div class="container">
+        <p>TESTO DI PROVA PER IL PUSH</p>
+    </div>
+</div>
 @endsection
