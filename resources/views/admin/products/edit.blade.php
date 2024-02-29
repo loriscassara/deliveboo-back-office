3eb73ff6c8a95d70ad4eb8efae80213c5152ce0e
DarioGuastella <jimmyguastella@gmail.com>
2024-02-29 15:50:16 +0100

    style: edit fix

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.admin')
-
 @section('content')
     <div class="container-fluid mt-4">
         <div class="row justify-content-center">
-            <h2>Nuovo Prodotto</h2>
+            <h2>Modifica Prodotto</h2>
             @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -19,59 +18,66 @@
                 @csrf
                 @method('PUT')
                 <div class="mb-3">
-                    <label for="name" class="form-label">Nome</label>
+                    <label for="name" class="form-label">Nome*</label>
                     <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                        name="name" value="{{ old('name') ?? $product->name }}">
+                        name="name" value="{{ old('name') ?? $product->name }}" required minlength="4"
+                        placeholder="Inserisci nome piatto">
                     @error('name')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
-
                 <div class="mb-3">
                     <label for="image" class="form-label">Immagine Prodotto</label>
-                    <img class="w-25 d-block" src="{{ asset('storage/' . $product->image) }}" alt="{{ 'Immagine' . ' ' . $product->name }}">
                     <input type="file" class="form-control @error('image') is-invalid @enderror" id="image"
-                        name="image" placeholder="choose an image" value="{{ old('image') }}">
+                        name="image" placeholder="Scegli un'immagine" value="{{ old('image') }}">
                     @error('image')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
-
                 <div class="mb-3">
-                    <label for="ingredients" class="form-label">Ingredienti</label>
+                    <label for="ingredients" class="form-label">Ingredienti*</label>
                     <input type="text" class="form-control @error('ingredients') is-invalid @enderror" id="ingredients"
-                        name="ingredients" value="{{ old('ingredients') ?? $product->ingredients }}">
+                        name="ingredients" value="{{ old('ingredients') ?? $product->ingredients }}" required minlength="4"
+                        placeholder="Inserisci lista ingredienti">
                     @error('ingredients')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
-
                 <div class="mb-3">
-                    <label for="description" class="form-label">Descrizione</label>
+                    <label for="description" class="form-label">Descrizione*</label>
                     <input type="text" class="form-control @error('description') is-invalid @enderror" id="description"
-                        name="description" value="{{ old('description') ?? $product->description }}">
+                        name="description" value="{{ old('description') ?? $product->description }}" required minlength="4"
+                        placeholder="Inserisci una descrizione">
                     @error('description')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
-
                 <div class="mb-3">
-                    <label for="price" class="form-label">Prezzo</label>
-                    <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"
-                        name="price" value="{{ old('price') ?? $product->price }}">
+                    <label for="price" class="form-label">Prezzo*</label>
+                    <input type="number" min="0" step=".01"
+                        class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+                        value="{{ old('price') ?? $product->price }}" placeholder="XXXX.XX" required>
                     @error('price')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
-                    <label for="visible" class="form-label">Visibile agli utenti</label>
-                    <input type="text" class="form-control @error('visible') is-invalid @enderror" id="visible"
-                        name="visible" value="{{ old('visible') ?? $product->visible }}">
-                    @error('visible')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
+                    <p>Visibile*</p>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                        <label class="form-check-label" for="visible">
+                            Si
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
+                            checked>
+                        <label class="form-check-label" for="visible">
+                            No
+                        </label>
+                    </div>
                 </div>
-
+                <p><i>*Campo obbligatorio</i></p>
                 <button type="submit" class="btn btn-primary">Salva Modifiche</button>
             </form>
         </div>
