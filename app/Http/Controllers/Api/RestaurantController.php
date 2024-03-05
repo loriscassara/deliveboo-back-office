1f976cebc9250ccbeae2abccd2369bb4c59c0e2c
DarioGuastella <jimmyguastella@gmail.com>
2024-03-05 18:20:04 +0100

    restaurant controller fix

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Restaurant;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
     public function index()
     {
-        $results = Restaurant::all();
+        $results = Restaurant::with('types')->get();
         $data = [
             "success" => true,
             "payload" => $results
@@ -20,7 +21,7 @@ class RestaurantController extends Controller
 
     public function show($id)
     {
-        $event = Restaurant::with(['products'])->find($id);
+        $event = Restaurant::with(['products', "types"])->find($id);
         return response()->json([
             "success" => $event ? true : false,
             "payload" => $event ? $event : "Nessun evento corrispondente all'id"
