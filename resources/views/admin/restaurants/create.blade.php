9b23312d7dfcb73871282bc149e90ec76a9cebff
DarioGuastella <jimmyguastella@gmail.com>
2024-03-11 12:46:17 +0100

    fix 1 type needed instead of 2

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container-fluid mt-4"> 
+    <div class="container-fluid mt-4">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card border-green">
