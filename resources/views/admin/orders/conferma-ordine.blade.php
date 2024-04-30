5bd03fe459843107ea57e9bbcbd83f52dd0eec59
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2024-04-30 11:01:56 +0200

    feat: view creation conferma-ordine

---
+++ resources/views/admin/orders/conferma-ordine.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conferma Ordine</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+        h1, p {
+            margin: 0;
+        }
+        img{
+            width: 50%;
+            border-radius: 30%;
+ 
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Gentile Cliente,</h1> <br>
+        <p>Grazie per aver effettuato un ordine su Deliveboo! Siamo felici di assisterti nel soddisfare le tue esigenze di ristorazione.</p><br><br>
+        <img src="https://static.winenews.it/2019/07/FoodDelivery.jpg" alt="">
+        <p>La tua soddisfazione è la nostra priorità e lavoreremo duramente per assicurarci che il tuo ordine venga consegnato in modo rapido e accurato.</p> <br><br>
+            
+        <p>Cordiali saluti,<br> <strong>Il Team di Deliveboo</strong></p>
+    </div>
+</body>
+</html>
